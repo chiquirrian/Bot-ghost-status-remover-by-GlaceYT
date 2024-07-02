26c199925e3a36cc4a0684561d60223a8f5fc04c
Dark <106180772+chiquirrian@users.noreply.github.com>
2024-07-02 19:26:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/**
+o/**
  ██████╗░████████╗██╗░░██╗           
  ██╔══██╗╚══██╔══╝╚██╗██╔╝          
  ██████╔╝░░░██║░░░░╚███╔╝░          
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["PLAYING","MUSIC"];
+const statusMessages = ["OrleansRP | EMS"];
 
 
 let currentIndex = 0;
